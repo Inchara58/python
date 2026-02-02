6b3007204c82b2024d0e0f7d4ddfba0682d52d25
Inchara58 <incharadr58@gmail.com>
2026-02-02 20:18:42 +0530

    chapter 2:variables,input,datatypes

---
+++ ch2.py
@@ -0,0 +1,21 @@
+name='Inchara'
+age=19
+is_student=True
+
+print(name)
+print(age)
+print(is_student)
+
+
+
+
+
+name=input(print("Enter your name"))
+science=int(input(print("Enter your scioence marks")))
+maths=int(input(print("enter your maths marks")))
+total=science+maths
+average=total/2
+
+print("Student",name)
+print("total",total)
+print("average",average)
