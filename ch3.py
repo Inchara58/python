699b01bd5b76b653b13c8430ba441731154b0739
Inchara58 <incharadr58@gmail.com>
2026-02-03 19:35:28 +0530

    chap 3 complted

---
+++ ch3.py
@@ -0,0 +1,15 @@
+"""num=int(input("enter a number"))
+if num%2==0:
+    print("even")
+else:
+    print("odd")  """  
+
+"""marks=int(input("Enter marks"))
+if marks>=35:
+    print("pass")
+else:
+    print("fail")  """  
+
+salary=5000
+salary+=2000
+print(salary)
