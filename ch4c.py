447126882b4a02e954c950e3f0845029128ffa1a
Inchara58 <incharadr58@gmail.com>
2026-02-04 19:34:33 +0530

    practice

---
+++ ch4c.py
@@ -0,0 +1,14 @@
+name="inchara"
+marks=43
+print(f"helloe{name},your marks is{marks}")
+
+"""formating nos"""
+
+cgpa=7.3456
+print("your cgpa is {cgpa:.2f}")
+
+
+"""string concat"""
+first="Helloe"
+second="world"
+print(first+""+second)
