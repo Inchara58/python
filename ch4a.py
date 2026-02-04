a2ab76f152562d2a06b91363f8f56614afcb9564
Inchara58 <incharadr58@gmail.com>
2026-02-04 19:19:14 +0530

    chap 4 a cpmplte string functions

---
+++ ch4a.py
@@ -0,0 +1,13 @@
+name="Inchara"
+new_name="i"+name[1:]
+print(new_name)
+
+print(name.upper())
+print(name.lower())
+
+place="  mysore   "
+print(place.strip())
+
+msg="i am great"
+print(msg.replace("great","good"))
+
