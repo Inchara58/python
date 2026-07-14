2d71c58de7839461b5d65506c6283f7d89808ff7
Inchara58 <incharadr58@gmail.com>
2026-07-14 22:30:22 +0530

    study

--- p.py
+++ p.py
@@ -1,7 +1,31 @@
+a=(int(input("Enter a number a:")))
+b=(int(input("Enter a number b:")))
 
+sum=a+b
+print( "The sum is :",sum)
 
 a=int(input("Enter the length:"))
 b=int(input("Enter the breadth:"))
 
 area=a*b
-print("area is ",area)
+perimeter=2*(a+b)
+print("area is {area}")
+print("perimeter is {perimeter}")
+
+
+1.e=10
+f=20
+print(e==f)
+
+2.c=20
+d=30
+print(d>c)
+
+3.student=24
+print(student>18)
+
+4.marks=32
+print(marks>35)
+
+5.age=70
+print(age>=60)
